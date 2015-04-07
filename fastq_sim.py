c850118dea2ab74d3d8ae00ae1e4900b564fcb90
Justin Bricker <jt.bricker@gmail.com>
2015-04-07 14:39:41 -0400

    Added induce_error function

--- fastq_sim.py
+++ fastq_sim.py
@@ -1,5 +1,8 @@
 #!/usr/local/bin/python
 from numpy.random import poisson
+from random import random
+
+bases = ['A','T','C','G']
 
 #---------Function: assign_error
 # 
@@ -8,17 +11,37 @@ from numpy.random import poisson
 # 
 #	TODO: Needs a more accurate method of calculating error
 def assign_error(fasta, lamb):
-	global errors
 	errors = []
 	for base in fasta:
 		errors.append(poisson(lamb)/100.0)
 	return errors
 
+
+#---------Function: induce_error
+# 
+#	Input: Raw FASTA Sequence (fasta), Associated Array of Errors (errors)
+#	Output: New FASTA Sequence with added errors (new_fasta)
+# 
+#	TODO: 
+def induce_error(fasta, errors):
+	new_fasta = ""
+	for i in range(len(fasta)):
+		if random() < errors[i]:
+			new_base = bases[int(random()*4)]
+			while new_base == fasta[i]:
+				new_base = bases[int(random()*4)]
+			new_fasta += new_base
+		else:
+			new_fasta += fasta[i]
+	return new_fasta
+
+
 def main():
-	fasta_raw='ATATCGGATCTAGGATTCGGATTAGGAATTCGAAA'	
-	mean_error = 2  #Assume Sequencing Machine has 2% error
+	fasta_raw='AAAAAAAAAA'	
+	mean_error = 20  #Assume Sequencing Machine has 2% error
 	errors = assign_error(fasta_raw, mean_error)
-	print errors
+	new_fasta = induce_error(fasta_raw,errors)
+	print new_fasta
 
 if __name__ == '__main__':
 	main()
