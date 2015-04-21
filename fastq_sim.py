21b2a1ff26da8d45b0063d2170fc043c7e0d81e1
Justin Bricker <jt.bricker@gmail.com>
2015-04-21 13:24:09 -0400

    v1.0 complete

--- fastq_sim.py
+++ fastq_sim.py
@@ -1,25 +1,41 @@
 #!/usr/local/bin/python
-from numpy.random import poisson
+from numpy.random import poisson, lognormal
 from random import random
 from math import log
 from Bio import SeqIO
+import sys
 
 bases = ['A','T','C','G']
 
+#---------Function: read_fasta
+# 
+#	Input: (filename) Name of fasta file
+#	Output: (fas) Array of fasta objects from Biopython 
+#
+#	Notes:  Returns an array of Seq objects from BioPython module.  Can extract
+#			sequence with fas[0].seq, or name with  fas[0].name
+# 
+#	TODO: 	
+#		Try and Except opening file
+#		Test if output file already exists and warn that it will be deleted?
+#		
+def read_fasta(filename):
+	fas = SeqIO.parse(open(filename,'r'),'fasta')
+	return fas
+
 #---------Function: assign_error
 # 
-#	Input: Raw FASTA Sequence (fasta), Mean of Poisson Distribution (lamb)
-#	Output: Array of error values associated with each base in fasta seq (errors)
+#	Input: (fasta) Raw FASTA Sequence, (mu) Mean of Errors 
+#	Output: (errors) Array of error values associated with each base in fasta seq 
 # 
 #	TODO: 
 #		Needs a more accurate method of calculating error
 #		Perhaps assign QUAL score instead of error
-def assign_error(fasta, lamb):
+def assign_error(seq, mu, sigma=1):
 	errors = []
-	for base in fasta:
-		errors.append(poisson(lamb)/100.0)
+	for i in range(len(seq)):
+		errors.append(lognormal(log(mu), sigma)) 
 	return errors
-
 
 #---------Function: induce_error
 # 
@@ -49,31 +65,47 @@ def induce_error(fasta, errors):
 #		Add compatibility with other sequencers
 def create_quality_string(errors):
 	fastq = ""
-	for error in errors:
+	for i in range(len(errors)):
+		error = errors[i]
 		QUAL = int(round(-10*log(error/(1-error),10)))   #These are mappings for Illumina 1.8+
 		fastq += chr(QUAL+33)
 	return fastq
 
-#---------Function: read_fasta
+#---------Function: output_fastq_file
 # 
-#	Input: (filename) Name of fasta file
-#	Output: (fas) Array of fasta objects from Biopython 
+#	Input: (new_seqs) Array of Errors , (quality_string) String of FASTQ ASCII characters
+#	Output: --none-- Method Creates FASTQ file of modified sequence data
 # 
 #	TODO: 
 #		
-def read_fasta(filename):
-	fas = SeqIO.parse(open(filename,'r'),'fasta')
-	return fas
+def output_fastq_file(outfile, name, sequence_string, quality_string):
+	outfile.write('@'+name+"\n")
+	outfile.write(sequence_string + "\n")
+	outfile.write('+\n')
+	outfile.write(quality_string+'\n')
+	return 
 
-def main():
-	fasta_raw='AAAAAAAAAA'	
-	mean_error = 20  #Assume Sequencing Machine has 2% error
+#---------Function: main
+# 
+#	Input: (filename) string indicating the name of the FASTA file you want to simulate on 
+#	Output: --none-- Runs all methods and creates FASTQ file of modified sequence data
+# 
+#	TODO: Add optional arguments (sequencing machine or errors for example)
+#		
+def main(filename):
+	mean_error = 0.02  #Assume Sequencing Machine has 2% error
 	
-	fastaread_fasta('example.fasta')
-	errors = assign_error(fasta_raw, mean_error)
-	new_fasta = induce_error(fasta_raw,errors)
-	fastq_quality_string = create_quality_string(errors)
-	
+	sequences = read_fasta(filename)
+	outfile = open(filename + ".FASTQ",'w')
+	for sequence in sequences:
+		sequence.errors = assign_error(sequence.seq, mean_error)
+		sequence.new_seq = induce_error(sequence.seq,sequence.errors)
+		sequence.qual = create_quality_string(sequence.errors)
+		output_fastq_file(outfile, str(sequence.name), str(sequence.seq), str(sequence.qual))
+	outfile.close()
 
 if __name__ == '__main__':
-	main()
+	if len(sys.argv) < 2:
+		print "Usage: fastq_sim file.fasta"
+		exit(1)
+	main(sys.argv[1])
---
+++ test_fastq_sim.py
@@ -0,0 +1,174 @@
+import unittest, fastq_sim as fs, os
+from math import log
+from scipy import stats
+from random import random
+
+
+bases = ['A','T','C','G']
+quals = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHI"
+
+#Test to Make Sure FASTA Files with Multiple Sequences are being read in correctly
+class ReadFASTATest(unittest.TestCase):
+	def setUp(self):
+		self.file = open('test.fa','w')
+		for a in range(3):
+			self.file.write('>TestSequence%s\n'%(a+1))
+			self.file.write(bases[a]*(a+1)*10)
+			self.file.write('\n')
+		self.file.close()
+
+	def test_read_first_sequence(self):
+		self.seqs = fs.read_fasta('test.fa')
+		self.assertEqual(self.seqs.next().seq,'AAAAAAAAAA')
+
+	def test_read_second_sequence(self):
+		self.seqs = fs.read_fasta('test.fa')
+		self.seqs.next()
+		self.assertEqual(self.seqs.next().seq,'TTTTTTTTTTTTTTTTTTTT')
+
+	def test_read_third_sequence(self):
+		self.seqs = fs.read_fasta('test.fa')
+		self.seqs.next()
+		self.seqs.next()
+		self.assertEqual(self.seqs.next().seq,'CCCCCCCCCCCCCCCCCCCCCCCCCCCCCC')
+
+	def tearDown(self):
+		os.remove('test.fa')
+
+# Test to Make Sure that the assign_error function is opererating correctly
+class AssignErrorTest(unittest.TestCase):
+	def setUp(self):
+		self.error_mean = 0.02;
+		self.seq = 'AAA'*10000
+		self.errors = fs.assign_error(self.seq, self.error_mean)
+
+	#test if length of error array is same as original sequence
+	def test_error_assign_length(self):
+		self.assertEqual(len(self.errors), len(self.seq))
+
+	# test if mean of errors is within a close range of true error mean.
+	def test_error_assign_mean(self):  
+		mean_of_errors = 0
+		for error in self.errors:
+			mean_of_errors += log(error)
+		mean_of_errors /= len(self.errors)
+		self.assertLess(abs(mean_of_errors-log(self.error_mean)), 0.01) 
+
+#Test if error induction method results in a mutated string with teh appropriate error rate
+class ErrorInductionTest(unittest.TestCase):
+	def setUp(self):
+		self.sequence = 'A'*10000
+		self.error_rate = 0.05
+		self.errors = fs.assign_error(self.sequence, self.error_rate*100.0)
+		self.error_seq = fs.induce_error(self.sequence, self.errors)
+
+	#test if percent of errors in new sequence agrees with the error rate
+	def test_error_induced_percent(self):
+		num_error = 0
+		for base in self.error_seq:
+			if base != 'A':
+				num_error +=1
+		self.assertLess(stats.binom_test(num_error, len(self.error_seq), self.error_rate*3/4.0), 0.05) #Binomial test with 5% significance
+		 # 3/4 factor to account for errors with no change (A -> A)
+
+	#test if length of error sequence is same as original sequence
+	def test_error_induced_length(self):
+		self.assertEqual(len(self.error_seq), len(self.sequence))		
+
+
+# Test to make sure quality string method is outputting the correct result
+class QualityAssignTest(unittest.TestCase):
+	def setUp(self):
+		self.errors = []
+		self.expected = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHI"
+		for Q in range(41):
+			self.errors.append(1.0/(1.0+10.0**(Q/10.0)))
+		self.fastq_string = fs.create_quality_string(self.errors)
+
+	#test if correct ascii characters are generated for Illumina 1.8+ with errors in full range
+	def test_illumina_ascii(self):
+		self.assertEqual(self.fastq_string,self.expected)
+
+	#test if the quality string is the correct length
+	def test_quality_string_length(self):
+		self.assertEqual(len(self.fastq_string),len(self.expected))
+
+# Test to make sure output as FASTQ function works properly
+class FastqOutputTest(unittest.TestCase):
+	def setUp(self):
+		self.filename = 'testfile.fasta'
+		self.seq_name = 'TestSequence'
+		self.error_seq = 'ATCCGATCCG'
+		self.qualityString = ''
+		for base in self.error_seq:  #Random QUALS
+			self.qualityString += quals[int(random()*len(self.error_seq))]
+		
+		self.outfile = open(self.filename+".FASTQ",'w')
+		fs.output_fastq_file(self.outfile, self.seq_name, self.error_seq, self.qualityString) #outputs to testfile.fasta.FASTQ
+		self.outfile.close()
+
+		self.file = open('testfile.fasta.FASTQ','r')
+		self.name_line = self.file.readline().strip()
+		self.seq_line = self.file.readline().strip()
+		self.plus_line = self.file.readline().strip()
+		self.qual_line = self.file.readline().strip()
+		self.file.close()
+
+	#test if the name line in FASTQ file is correct
+	def test_fastq_out_name(self):
+		self.assertEqual(self.name_line, "@"+self.seq_name)
+
+	#test if the sequence line in FASTQ file is correct
+	def test_fastq_out_seq(self):
+		self.assertEqual(self.seq_line, self.error_seq)
+
+	#test if the plus line in FASTQ file is correct
+	def test_fastq_out_plus(self):	
+		self.assertEqual(self.plus_line, '+')
+
+	#test if the quality string line in FASTQ file is correct
+	def test_fastq_out_quals(self):
+		self.assertEqual(self.qual_line, self.qualityString)
+
+	def tearDown(self):
+		os.remove('testfile.fasta.FASTQ')
+
+#Test to make sure output as FASTQ function works properly when multiple sequences present
+class MainTest(unittest.TestCase):
+	def setUp(self):
+		#Generate a fasta file with multiple sequences
+		self.filename = 'testfile.fasta'
+		self.num_seq = 10
+		self.infile = open('testfile.fasta','w')
+		for i in range(self.num_seq):
+			self.infile.write(">TestSeq%s\n"%(i+1))
+			seq = bases[(i%4)]*50
+			self.infile.write("%s\n"%seq)
+		self.infile.close()
+
+		#Pass fasta through the main method
+		fs.main(self.filename)
+
+		#Observe the output file and check
+		self.outfile = open('testfile.fasta.FASTQ','r')
+		self.seqs = self.outfile.readlines()
+		self.outfile.close()
+
+	#test if the correct number of lines appear in the output file
+	def test_num_output_lines(self):
+		self.assertEqual(len(self.seqs),4*self.num_seq)
+
+	#test if the 5th line in the output file is the correct name of the 2nd sequence
+	def test_second_name(self):
+		self.assertEqual(self.seqs[4].strip(),"@TestSeq2")
+
+	#test if the 11th line in the output file contains only '+'
+	def test_third_plus(self):
+		self.assertEqual(self.seqs[10].strip(),'+')
+
+	def tearDown(self):
+		os.remove('testfile.fasta')
+		os.remove('testfile.fasta.FASTQ')
+
+if __name__ == '__main__':
+	unittest.main()
