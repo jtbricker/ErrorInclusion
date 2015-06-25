9e8046551b7e8ac1637777ddc1f6b58ee57ca0fe
Justin Bricker <jt.bricker@gmail.com>
2015-06-25 09:03:30 -0400

    updated

---
+++ clean.sh
@@ -0,0 +1,14 @@
+d="data"
+rm -f $d/*.FASTA
+rm -f $d/*.fasta
+rm -f $d/*.phy
+rm -f $d/*.FASTQ
+rm -f $d/*.fastq
+rm -f $d/*fastqc*
+rm -f $d/*.trees
+rm -f $d/*.tre
+rm -f $d/*.nex
+rm -f $d/*.log
+rm -f $d/beauti_template.xml.state
+rm -f $d/*sequences*
+rm -f $d/*.log
