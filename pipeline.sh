dc58331ad5512d12535a58b72133e23221978b53
Justin Bricker <jt.bricker@gmail.com>
2015-06-25 09:03:12 -0400

    New Steps

--- pipeline.sh
+++ pipeline.sh
@@ -8,35 +8,42 @@ scripts="scripts"
 #	-o (output type) Phylip
 #	-q (quiet)
 #	-l (length) 1000
-seq-gen -mHKY $data/example.tree -op -q -l1000 >> $data/sequences.phy
+seq-gen -mHKY $data/example.tree -op -q -l1000 >> $data/inseq.phy
 
 #Step 2:(ABC) Convert seqeunces from phylip to FASTA format
 #	Input: $data/sequences.phy
 #	Output: $data/sequences.fasta
-python $scripts/phylipToFASTA.py $data/sequences.phy $data/sequences.fasta
+python $scripts/phylipToFASTA.py $data/inseq.phy $data/inseq.fasta
 
 #Step 3:(BC) Generate simulated FASTQ file from the FASTA file.
-python $scripts/fastq_sim.py $data/sequences.fasta
+python $scripts/fastq_sim.py $data/inseq.fasta
 
 #Step 4:(B) Run FASTQ file through some preprocessor
-
+#stacks  preprocessor
 #Step 4b:(B) Convert FASTQ to FASTA
 
 #Step 5:(ABC) Generate XML file for beast using FASTA/FASTQ files
+#cat beauti_template.xml | sed 's/nameofFASTAfile/newName/g' > newNameBeauti.xml  #copying the  beauti file
+
 
 #Step 6:(ABC) Run beast
 beast -overwrite -working $data/beauti_template.xml
 
 #Step 7:(ABC) Annotate trees
-treeannotator $data/sequences.trees $data/treeA.nex
+# treeannotator $data/inseq.trees $data/treeA.nex
 
 #Step 8:(ABC) Convert trees into newick format
-phyutility -vert -in $data/treeA.nex -out $data/treeA.tre
+# cat out.tree | grep ^tree | tr '[]' '\n' | grep -v ^\& | tr -d ' \n' | sed 's/^tree.*=//' > $data/treeA.tre
 
 #Step 8.5: Concat trees together
-cat $data/example.tree >> $data/treeABC.tre
-cat $data/treeA.tre >> $data/treeABC.tre
+# cat $data/example.tree >> $data/treeABC.tre
+# cat $data/treeA.tre >> $data/treeABC.tre
 # cat treeB.tre >> treeABC.tre
 # cat treeC.tre >> treeABC.tre
 
 #Step 9:(ABC) Measure tree distance
+#printf 'Y\n' | treedist 
+
+#geodesic
+#mary kuhner
+#
