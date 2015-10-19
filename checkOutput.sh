95624c2af2105f22751531f1b6b7ec7cd898c01a
Justin Bricker <jt.bricker@gmail.com>
2015-10-19 09:41:22 -0400

    Fixed for new run

--- checkOutput.sh
+++ checkOutput.sh
@@ -2,13 +2,13 @@
 
 for E in 0.01 0.05 0.10
 do
-	for L in 100 500 1000
+	for L in 50 100 500
 	do
 		for R in {1..100}
 		do
 			filename=$R\_E$E\_L$L
 
-			for T in RAW FASTQ FASTQ\_FILTERED
+			for T in RAW FASTA_ERROR FILTERED FASTQ_CALCULATED
 			do
 			[ ! -f $filename\_inseq\_$T.trees ] && echo "cd ErrorInclusion; beast_submit beast data/$R""_E$E""_L$L""_inseq_""$T""_beauti.xml $R""_E$E""_L$L""_$T 1 4:00:00" >> ../launchfile_redo
 			done
